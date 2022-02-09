915e453d8ec3ad1c06ccef21adfeaaf4a4fef1a7
adriankosmala1998 <94392575+adriankosmala1998@users.noreply.github.com>
2022-02-09 23:05:18 +0100

    Delete npd_final_project directory

--- npd_final_project/npd_final_project/Readme.rst
+++
@@ -1,1 +0,0 @@
-Biblioteka 'npd_final_project' służy do analizy dochodu z podatku PIT dla poszczególnych Jednostek Samorządu Terytorialnego.
--- npd_final_project/npd_final_project/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="npd_final_project",
-    version="0.1",
-    py_modules=["import_data", "calculate_statistics", "create_output"] # use when you would like to include modules to python package
-)
-
